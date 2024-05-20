07a7d980ec75dd6d8d43996d6decd1638ba8411a
in-up <inup@kyonggi.ac.kr>
2024-05-21 01:08:28 +0900

    :memo: 즐겨찾기 변경 -즐겨찾기 버튼 누르면 안내 메세지 띄운 후 -음성 인식 창으로 이동

--- lib/data/routes.dart
+++ lib/data/routes.dart
@@ -1,5 +1,7 @@
 import 'package:busyoong/screen/result.dart';
 import 'package:busyoong/screen/search.dart';
+import 'package:busyoong/screen/favorite.dart';
+import 'package:busyoong/screen/favorite_search.dart';
 
 import 'package:flutter/material.dart';
 import '../screen/home.dart';
@@ -10,5 +12,6 @@ final Map<String, WidgetBuilder> routes = {
   '/home' : (context) => HomeScreen(),
   '/search' : (context) => SearchScreen(),
   '/result' : (context) => ResultScreen(),
-
+  '/favorite' : (context) => FavoriteScreen(),
+  '/favorite_search' : (context) => FavoriteSearchScreen(),
 };
