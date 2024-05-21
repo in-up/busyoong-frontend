dcf9e44eeb6e5779b529afc34a3faac296955dd0
in-up <inup@kyonggi.ac.kr>
2024-05-22 07:46:59 +0900

    feat: add search_result.dart

---
+++ lib/screen/search_result.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class SearchResultsPage extends StatefulWidget {
+  final String refinedString;
+
+  SearchResultsPage({Key? key, required this.refinedString}) : super(key: key);
+
+  @override
+  _SearchResultsPageState createState() => _SearchResultsPageState();
+}
+
+class _SearchResultsPageState extends State<SearchResultsPage> {
+  Future<List<String>> fetchSearchResults(String query) async {
+    return List.generate(10, (index) => 'Search Result ${index + 1}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Search Results'),
+      ),
+      body: FutureBuilder(
+        future: fetchSearchResults(widget.refinedString),
+        builder: (context, AsyncSnapshot<List<String>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index]),
+                  trailing: IconButton(
+                    icon: Icon(Icons.favorite_border),
+                    onPressed: () {
+                      // Implement favorite functionality here
+                    },
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
